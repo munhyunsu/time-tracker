5e373d5c9b324adc492e1c431dde90d5ca3528cf
munhyunsu <munhyunsu@gmail.com>
2021-03-28 09:52:11 +0900

    linux setup

--- setup.py
+++ setup.py
@@ -30,20 +30,24 @@ def main():
     print(f'\x1B[32mmain.py path: {main_path}\x1B[0m')
     print('\x1B[47m\x1B[32mCheck Python3 interpreter path done\x1B[0m')
 
-    print('\x1B[32mCreate timetracker.desktop\x1B[0m')
-    desktop = (f'[Desktop Entry]\n'
-               f'Name=Time Tracker\n'
-               f'Comment=Time Tracker\n'
-               f'Exec=bash -c "{python3_path} {main_path}"\n'
-               f'Terminal=false\n'
-               f'Type=Application\n'
-               f'Categories=Office;\n')
-    print(f'\x1B[32m{desktop}\x1B[0m')
-    desktop_path = os.path.join(root_dir, 'timetracker.desktop')
-    with open(desktop_path, 'w') as f:
-        f.write(desktop)
-    print(f'\x1B[47m\x1B[32mln -s {desktop_path} ~/.local/share/applications/\x1B[0m')
-    print(f'\x1B[47m\x1B[32mOr move timetracker.desktop to ~/.local/share/applications/\x1B[0m')
+    if sys.platform == 'linux':
+        print('\x1B[32mCreate timetracker.desktop\x1B[0m')
+        desktop = (f'[Desktop Entry]\n'
+                   f'Name=Time Tracker\n'
+                   f'Comment=Time Tracker\n'
+                   f'Exec=bash -c "{python3_path} {main_path}"\n'
+                   f'Terminal=false\n'
+                   f'Type=Application\n'
+                   f'Categories=Office;\n')
+        print(f'\x1B[32m{desktop}\x1B[0m')
+        desktop_path = os.path.join(root_dir, 'timetracker.desktop')
+        with open(desktop_path, 'w') as f:
+            f.write(desktop)
+        print(f'\x1B[47m\x1B[32mln -s {desktop_path} ~/.local/share/applications/\x1B[0m')
+        print(f'\x1B[47m\x1B[32mOr move timetracker.desktop to ~/.local/share/applications/\x1B[0m')
+    elif sys.platform == 'cygwin':
+        print('\x1B[32mNeed to execution bash on Windows\x1B[0m')
+
 
     print('\x1B[1m\x1B[33mTime Tracker Installation Complete\x1B[0m')
 
